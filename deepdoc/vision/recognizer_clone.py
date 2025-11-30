f872988a157ee54cee9475063767a30e490dc840
ducmai444 <ducmai4203@gmail.com>
2025-11-30 22:57:47 +0700

    chore:

--- deepdoc/vision/recognizer_clone.py
+++ deepdoc/vision/recognizer_clone.py
@@ -48,5 +48,58 @@ class Recognizer:
     def sort_C_firstly(arr, thr=0):
         # sort using y1 first and then x1
         # sorted(arr, key=lambda r: (r["x0"], r["top"]))
-        arr = Recognizer.sort_X()
-        pass
+        arr = Recognizer.sort_X_firstly(arr, thr)
+        for i in range(len(arr) - 1):
+            for j in range(i, -1, -1):
+                # restore the order using th
+                if "C" not in arr[j] or "C" not in arr[j+1]:
+                    continue
+                if arr[j+1]["C"] < arr[j]["C"] or \
+                    (arr[j+1]["C"] == arr[j]["C"] and
+                        arr[j+1]["top"] > arr[j]["top"]):
+                    tmp = arr[j]
+                    arr[j] = arr[j+1]
+                    arr[j+1] = tmp
+        return arr
+    
+    @staticmethod
+    def sort_R_firstly(arr, thr=0):
+        # sort using y1 first and then x1
+        # sorted(arr, key=lambda r: (r["top"], r["x0"]))
+        arr = Recognizer.sort_Y_firstly(arr, thr)
+        for i in range(len(arr) - 1):
+            for j in range(i, -1, -1):
+                if "R" not in arr[j] or "R" not in arr[j + 1]:
+                    continue
+                if arr[j + 1]["R"] < arr[j]["R"] \
+                        or (
+                        arr[j + 1]["R"] == arr[j]["R"]
+                        and arr[j + 1]["x0"] < arr[j]["x0"]
+                ):
+                    tmp = arr[j]
+                    arr[j] = arr[j + 1]
+                    arr[j + 1] = tmp
+        return arr
+    
+    @staticmethod
+    def overlapped_area(a, b, ratio=True):
+        tp, btm, x0, x1 = a["top"], a["bottom"], a["x0"], a["x1"]
+        if b["x0"] > x1 or b["x1"] < x0:
+            return 0
+        if b["bottom"] < tp or b["top"] > btm:
+            return 0
+        x0_ = max(b["x0"], x0)
+        x1_ = min(b["x1"], x1)
+        assert x0_ <= x1_, "Bbox mismatch! T:{}, B:{}, x0:{}, x1:{} ==> {}".format(
+            tp, btm, x0, x1, b)
+        tp_ = max(b["top"], tp)
+        btm_ = min(b["bottom"], btm)
+        assert tp_ <= btm_, "Bbox mismatch! T:{}, B:{}, x0:{}, x1:{} ==> {}".format(
+            tp, btm , x0, x1, b)
+        ov = (btm_ - tp_) * (x1_ - x0_) if x1 - x0 != 0 \
+            and btm - tp != 0 else 0
+        if ov > 0 and ratio:
+            ov /= (x1 - x0) * (btm - tp)
+        return ov
+
+    
