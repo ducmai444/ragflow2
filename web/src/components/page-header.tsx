4f43cd24eb8cabf54d5b56e6103a3f36e9887649
ducmai444 <ducmai4203@gmail.com>
2025-12-01 22:42:53 +0700

    chore:

--- web/src/components/page-header.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-export function PageHeader({ children }: PropsWithChildren) {
-  return (
-    <header className="flex justify-between items-center border-b bg-text-title-invert p-5">
-      {children}
-    </header>
-  );
-}
